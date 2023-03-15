1ccaa4f507659db9a164cbc29d39b92554be585a
icika-max <icikakennedy@gmail.com>
2023-03-15 15:50:14 +0300

    completed

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(()=>{
+    fetch((r)=>r.json())
+    .then((movies)=>console.log(movies))
+  },[])
+  return (
+    <div className="App">
+      <h1>Hello from React!</h1>
+    </div>
+  );
+}
+
+export default App;
